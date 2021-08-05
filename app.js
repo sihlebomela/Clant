4f064cc314d9eac702a13056229f3eff19b3c629
trinity-senpai <sihletrinity70@gmail.com>
2021-08-05 16:20:50 +0200

    take picture function

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ function setup() {
 function draw() {
     capture.size(windowWidth, windowHeight);
 }
+
+//function takes a picture
+function takePicture() {
+    capture.loadPixels();
+    captureFrameData = capture.canvas.toDataURL(); //get frame and convert to base64
+    return captureFrameData;
+}
