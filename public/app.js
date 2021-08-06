7f15cc3cb2e41d9bf906add606460afad4962c0e
trinity-senpai <sihletrinity70@gmail.com>
2021-08-06 02:24:33 +0200

    feat: send image to server

--- public/app.js
+++ public/app.js
@@ -15,10 +15,15 @@ function draw() {
 ui.addEventListener('click', (ev) => {
   if(ev.target.parentElement.classList.contains('capture')) {
     sidebar.classList.add('show'); // show sidebar
+    
+    //send post request to endpoint and attach image
+    let base64Img = takePicture();
+    sendRequest(base64Img).then(res => {
+      console.log(res);
+    });
   } else if (ev.target.classList.contains('ui') || ev.target.classList.contains('logo')) {
     sidebar.classList.remove('show'); // show sidebar
   }
-  //todo: send image to identification
 })
 
 //function takes a picture
@@ -26,4 +31,17 @@ function takePicture() {
     capture.loadPixels();
     captureFrameData = capture.canvas.toDataURL(); //get frame and convert to base64
     return captureFrameData;
+}
+
+async function sendRequest(image) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(image)
+  };
+  const res = await fetch('/identify', options)
+  const json = res.json(); // convert to json
+  return json;
 }
