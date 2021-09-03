ace904483fb908e2c5ea2e65151f996d81254739
trinity-senpai <sihletrinity70@gmail.com>
2021-09-03 20:01:44 +0200

    update: port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 
 require("dotenv").config(); //config env
